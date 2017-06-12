12c4d64494e9edb19ff3417cac9b4842b37f1242
imcvampire <quocanh96@gmail.com>
2017-06-12 20:54:01 +0700

    feat: add resource

--- src/server/shared/team.py
+++ src/server/shared/team.py
@@ -3,6 +3,13 @@ class Team:
         self.id = id
         self.name = name
         self.members = []
+        self.resources = {
+            "gold": 0,
+            "iron": 0,
+            "stone": 0,
+            "wood": 0,
+        }
+        self.inventory = []
 
     def add_member(self, member):
         if len(self.members) >= 4:
@@ -11,3 +18,24 @@ class Team:
         self.members.append(member)
 
         return True
+
+    def add_resource(self, type, amount):
+        self.resources[type] += amount
+
+    def reduce_resource(self, type, amount):
+        if self.resources[type] < amount:
+            return False
+
+        self.resources[type] -= amount
+
+        return True
+
+    def add_item(self, item_id):
+        if item_id not in self.inventory:
+            self.inventory.append(item_id)
+            return True
+
+        return False
+
+    def buy_item(self, type, item_id):
+        return None
