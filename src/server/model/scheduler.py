cdf4fe01eeb9cf9f8589a763d9c71913cd3eb33a
imcvampire <quocanh96@gmail.com>
2017-06-16 10:26:52 +0700

    docs: scheduler

--- src/server/model/scheduler.py
+++ src/server/model/scheduler.py
@@ -4,6 +4,13 @@ from interval import VALUE
 
 
 def check_data(teams, fields, castles, init=False):
+    """Create new Timer, start it and check all server's data
+    :param teams: array of team
+    :param fields: array of field
+    :param castles: array of castle
+    :param init: whether is init or not
+    :return timer: a new timer
+    """
     timer = Timer(1.0, check_data, [
         teams,
         fields,
@@ -12,6 +19,7 @@ def check_data(teams, fields, castles, init=False):
 
     timer.start()
 
+    # We dont need run this block if this is init function
     if not init:
         for field in fields:
             for resource in RESOURCES[:2]:
